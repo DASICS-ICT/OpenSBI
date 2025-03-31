806ba2b263a05a0e5e2e1f5978051a401ec935c2
Skylake <lacrosseelis@gmail.com>
2025-03-31 12:54:25 +0800

    separate system.dts for qemu and v3achip

--- build_nemu.sh
+++ build_nemu.sh
@@ -1,3 +1,4 @@
-dtc -O dtb -o system.dtb ./dts/system.dts
-
+export PATH=/data/jinyue/riscv-toolchain/nanhu-v10.0.2/bin:$PATH
+export RISCV=/data/jinyue/riscv-toolchain/nanhu-v10.0.2
+dtc -O dtb -o system.dtb ./dts/system-qemu.dts
 make PLATFORM=generic CROSS_COMPILE=riscv64-unknown-linux-gnu- FW_FDT_PATH=system.dtb FW_PAYLOAD_PATH=../riscv-linux/arch/riscv/boot/Image -j16 
--- build_v3a.sh
+++ build_v3a.sh
@@ -1,3 +1,2 @@
-dtc -O dtb -o system.dtb ./dts/system.dts
-
+dtc -O dtb -o system.dtb ./dts/system-chip.dts
 make PLATFORM=generic CROSS_COMPILE=riscv64-unknown-linux-gnu- FW_FDT_PATH=system.dtb FW_PAYLOAD_PATH=../riscv-linux/arch/riscv/boot/Image -j16 
