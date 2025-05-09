340fd33dde8da3c29657c0c7124c9f8a1c529e5d
Yue Jin <jinyue20s@ict.ac.cn>
2025-05-09 13:53:46 +0800

    add build script for vcu128 fpga

--- build_v3a.sh
+++
@@ -1,2 +0,0 @@
-dtc -O dtb -o system.dtb ./dts/system-chip.dts
-make PLATFORM=generic CROSS_COMPILE=riscv64-unknown-linux-gnu- FW_FDT_PATH=system.dtb FW_PAYLOAD_PATH=../riscv-linux/arch/riscv/boot/Image -j16 
